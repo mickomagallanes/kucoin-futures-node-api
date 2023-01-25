353912a44175b527b356a76bd5ac7fffe70314fa
eikkuzai <enorrback1@gmail.com>
2023-01-25 15:14:22 +0200

    Update kucoin.js

--- kucoin.js
+++ kucoin.js
@@ -1,242 +1,63 @@
-const axios = require('axios')
+const qs = require('querystring')
+const crypto = require('crypto')
 
-const Market = {}
-
-/* 
-  Get Order Info. of the Contract
-  GET /api/v1/contracts
-  symbol = string [optional]
-*/
-Market.getContract = async function (symbol = "") {
-  let endpoint = `/api/v1/contracts/${symbol}`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Open Contract List
-  GET /api/v1/contracts
-  symbol = string [optional]
-*/
-Market.getAllContracts = async function () {
-  let endpoint = "/api/v1/contracts/active"
-
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/*  
-  Get Ticker
-  GET /api/v1/ticker
-  symbol = string
-*/
-Market.getTicker = async function (symbol) {
-  let endpoint = `/api/v1/ticker?symbol=${symbol}`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Full Order Book - Level 2 (aggregated)
-  see details at: https://docs.kucoin.com/futures/#get-full-order-book-level-2
-  GET /api/v1/level2/snapshot?symbol=<symbol>
-  symbol = string
-*/
-Market.getFullOrderBook = async function (symbol) {
-  let endpoint = `/api/v1/market/orderbook/level2?symbol=${symbol}`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Part Order Book (aggregated) 
-  GET /api/v1/level2/depth20?symbol=<symbol>
-  GET /api/v1/level2/depth100?symbol=<symbol>
-  params = {
-    amount: integer (20 || 100) 
-    symbol: string
-  }
-*/
-Market.getPartOrderBook = async function (params) {
-  let endpoint = `/api/v1/level2/depth${params.amount}?symbol=${params.symbol}`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Level 2 Pulling Messages
-  see details at: https://docs.kucoin.com/futures/#level-2-pulling-messages
-  GET /api/v1/level2/message/query
-  params = {
-    symbol: string
-    start: long
-    end	: long
-  }
-*/
-Market.pullMessagesLvl2 = async function (params) {
-  let endpoint = `/api/v1/level2/message/query`
-  let url = this.baseURL + endpoint + this.formatQuery(params)
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Full Order Book (atomic) 
-  GET /api/v2/level3/snapshot?symbol=<symbol>
-  symbol = string
-*/
-Market.getFullOrderBookAtomic = async function (symbol) {
-  let endpoint = `/api/v2/level3/snapshot?symbol=${symbol}`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Level 3 Pulling Messages
-  see details at: https://docs.kucoin.com/futures/#level-3-pulling-messages
-  GET /api/v1/level3/snapshot
-  params = {
-    symbol: string
-    start: long
-    end	: long
-  }
-*/
-Market.pullMessagesLvl3 = async function (params) {
-  let endpoint = `/api/v1/level3/snapshot`
-  let url = this.baseURL + endpoint + this.formatQuery(params)
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  List the last 100 trades for a symbol
-  GET /api/v1/trade/history?symbol=<symbol>
-  symbol = string
-*/
-Market.getTradeHistories = async function (symbol) {
-  let endpoint = `/api/v1/trade/history?symbol=${symbol}`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Interest Rate List
-  GET /api/v1/interest/query
-  params = {
-    symbol = string
-    startAt = long (unix time) [optional]
-    endAt = long (unix time) [optional]
-    reverse = boolean [optional, default: true]
-    offset = long [optional]
-    forward = boolean [optional, default: true]
-    maxCount = int [optional, default: 10]
+class Kucoin {
+  constructor() {
   }
 
-*/
-Market.getInterestRate = async function (params) {
-  let endpoint = `/api/v1/interest/query`
-  let url = this.baseURL + endpoint + this.formatQuery(params)
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Index List
-  GET /api/v1/index/query
-  params = {
-    symbol = string
-    startAt = long (unix time) [optional]
-    endAt = long (unix time) [optional]
-    reverse = boolean [optional, default: true]
-    offset = long [optional]
-    forward = boolean [optional, default: true]
-    maxCount = int [optional, default: 10]
+  init(config) {
+    let url = ''
+    if (config.environment === 'live') {
+      url = 'https://api-futures.kucoin.com'
+    } else {
+      url = 'https://api-sandbox-futures.kucoin.com'
+    }
+    this.environment = config.environment
+    this.baseURL = url
+    this.secretKey = config.secretKey
+    this.apiKey = config.apiKey
+    this.passphrase = config.passphrase
+    const User = require('./lib/user')
+    const Market = require('./lib/market')
+    const Trade = require('./lib/trade')
+    const Sockets = require('./lib/websockets')
+    Object.assign(this, User, Market, Trade, Sockets)
   }
-*/
-Market.getIndexList = async function (params) {
-  let endpoint = `/api/v1/index/query`
-  let url = this.baseURL + endpoint + this.formatQuery(params)
-  let result = await axios.get(url)
-  return result.data
-}
 
-/* 
-  Get Current Mark Price
-  GET /api/v1/mark-price/{symbol}/current
-  symbol = string
-*/
-Market.getMarkPrice = async function (symbol) {
-  let endpoint = `/api/v1/mark-price/${symbol}/current`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Premium Index
-  GET /api/v1/index/query
-  params = {
-    symbol = string
-    startAt = long (unix time) [optional]
-    endAt = long (unix time) [optional]
-    reverse = boolean [optional, default: true]
-    offset = long [optional]
-    forward = boolean [optional, default: true]
-    maxCount = int [optional, default: 10]
+  sign(endpoint, method, params = {}) {
+    let header = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    let nonce = Date.now() + ''
+    let strForSign = ''
+    if (method === 'GET' || method === 'DELETE') {
+      strForSign = nonce + method + endpoint + this.formatQuery(params)
+    } else {
+      strForSign = nonce + method + endpoint + JSON.stringify(params)
+    }
+    let signatureResult = crypto.createHmac('sha256', this.secretKey)
+      .update(strForSign)
+      .digest('base64')
+    let passphraseResult = crypto.createHmac('sha256', this.secretKey)
+      .update(this.passphrase)
+      .digest('base64')
+    header.headers['KC-API-SIGN'] = signatureResult
+    header.headers['KC-API-TIMESTAMP'] = nonce
+    header.headers['KC-API-KEY'] = this.apiKey
+    header.headers['KC-API-PASSPHRASE'] = passphraseResult
+    header.headers['KC-API-KEY-VERSION'] = 2
+    return header
   }
-*/
-Market.getPremiumIndex = async function (params) {
-  let endpoint = `/api/v1/index/query`
-  let url = this.baseURL + endpoint + this.formatQuery(params)
-  let result = await axios.get(url)
-  return result.data
-}
 
-/* 
-  Get Current Funding Rate
-  GET /api/v1/funding-rate/{symbol}/current
-  symbol = string
-*/
-Market.getFundingRate = async function (symbol) {
-  let endpoint = `/api/v1/funding-rate/${symbol}/current`
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Server Time
-  GET /api/v1/timestamp
-*/
-Market.getServerTime = async function () {
-  let endpoint = '/api/v1/timestamp'
-  let url = this.baseURL + endpoint
-  let result = await axios.get(url)
-  return result.data
-}
-
-/* 
-  Get Klines
-  GET /api/v1/kline/query
-  params = {
-    symbol: string [Required]
-    granularity: int [Required] - Granularity (minute)
-    from: long (unix time) [Optional]
-    to: long (unix time) [Optional]
+  formatQuery(queryObj) {
+    if (queryObj !== undefined && JSON.stringify(queryObj).length !== 2) {
+      return '?' + qs.stringify(queryObj)
+    } else {
+      return ''
+    }
   }
-*/
-Market.getKlines = async function (params) {
-  let endpoint = '/api/v1/kline/query'
-  let url = this.baseURL + endpoint + this.formatQuery(params)
-  let result = await axios.get(url)
-  return result.data
 }
 
-module.exports = Market
+module.exports = Kucoin
